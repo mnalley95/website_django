3204046c8a1e29de02e3e59c1fe016efef49cb38
mnalley95 <45980636+mnalley95@users.noreply.github.com>
2023-02-12 22:00:34 -0800

    test

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,6 +79,7 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+"""
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
@@ -89,7 +90,7 @@ DATABASES = {
         "PORT": "",
     }
 }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
