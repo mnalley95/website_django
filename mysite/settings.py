f618c226e4b48cf59841af21f087681f789cc6ed
mnalley95 <mnalley95@gmail.com>
2023-02-13 21:42:03 -0800

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,7 +79,6 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-"""
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
@@ -89,7 +88,6 @@ DATABASES = {
         "PASSWORD": os.environ.get("DBPASS"),
     }
 }
-"""
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
